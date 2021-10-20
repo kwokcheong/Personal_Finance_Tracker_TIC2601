b040c5dd53a1b252f6748af79fd0dedbb9ee0e5f
directorkang <88839930+directorkang@users.noreply.github.com>
2021-10-20 22:28:58 +0800

    Update prototype database and query.sql

--- database/prototype database and query.sql
+++ database/prototype database and query.sql
@@ -16,8 +16,10 @@ CREATE TABLE users (
 );
 
 CREATE TABLE incomes (
+    incomeID VARCHAR(256) NOT NULL PRIMARY KEY,
 	userID INTEGER REFERENCES users(userID) ON DELETE CASCADE ON UPDATE CASCADE,
-    amount DECIMAL(13,2) CHECK(amount >= 0), 
+    name VARCHAR(256) NOT NULL,
+    amount DECIMAL(13,2) CHECK(amount >= 0),
     category VARCHAR(64),
     recurring_date DATE,
     recurring BOOLEAN NOT NULL DEFAULT 0,
@@ -25,6 +27,7 @@ CREATE TABLE incomes (
 );
 
 CREATE TABLE expenses (
+    expensesID VARCHAR(256) NOT NULL PRIMARY KEY,
 	userID INTEGER REFERENCES users(userID) ON DELETE CASCADE ON UPDATE CASCADE,
     name VARCHAR(256) NOT NULL,
     amount DECIMAL(13,2) CHECK(amount >= 0),
@@ -41,11 +44,13 @@ CREATE TABLE ledger (
 );
 
 CREATE TABLE goals (
+    goalID VARCHAR(256) NOT NULL PRIMARY KEY,
 	userID INTEGER REFERENCES users(userID) ON DELETE CASCADE ON UPDATE CASCADE,
     name VARCHAR(256) NOT NULL,
     amount DECIMAL(13,2) CHECK(amount >= 0),
     category VARCHAR(64),
     possible BOOLEAN DEFAULT 0,
+    done BOOLEAN DEFAULT 0,
     start_date DATE,
     end_date DATE,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
@@ -59,33 +64,34 @@ CREATE TABLE budgethead (
 );
 
 
-INSERT INTO users VALUES (1, 'admin', 'admin@hotmail.com', 'admin123'); 
+INSERT INTO users VALUES (2, 'Root', 'root@gmail.com', 'root123'); 
 
-INSERT INTO incomes (userID,amount, category, recurring_date,recurring) 
-VALUES (1, 3250.50, 'monthly salary', '2021-10-10', 1); 
+INSERT INTO incomes (incomeID, userID,amount, name, category, recurring_date,recurring) 
+VALUES (0, 2, 3250.50, 'monthly salary', 'monthly salary', '2021-10-10', 1); 
 
-INSERT INTO expenses(userID, name, amount, category, created_at)
-VALUES(1, 'Hokkien Mee', 5, 'food', '2021-10-18');
+INSERT INTO expenses(expensesID, userID, name, amount, category, created_at)
+VALUES(3, 2, 'Hokkien Mee', 5, 'food', '2021-10-18');
 
-INSERT INTO expenses(userID, name, amount, category, created_at)
-VALUES(1, 'Laksa', 2, 'food', '2021-10-18');
+INSERT INTO expenses(expensesID, userID, name, amount, category, created_at)
+VALUES(4, 2, 'Laksa', 2, 'food', '2021-10-18');
 
 UPDATE expenses
 SET amount = 5
-WHERE userID = 1 AND created_at = '2021-10-18'; 
+WHERE userID = 2 AND created_at = '2021-10-18'; 
 
-INSERT INTO expenses(userID, name, amount, category, created_at)
-VALUES(1, 'Laksa', 5, 'food', '2021-10-18');
+INSERT INTO expenses(expensesID, userID, name, amount, category, created_at)
+VALUES(5, 2, 'Laksa', 5, 'food', '2021-10-18');
 
-INSERT INTO expenses(userID, name, amount, category, created_at)
-VALUES(1, 'bubbletea', 6.7, 'drink', '2021-10-18');
+INSERT INTO expenses(expensesID, userID, name, amount, category, recurring_date, recurring)
+VALUES(6, 2, 'bubbletea', 6.7, 'drink','2021-10-16', 1);
 
-INSERT INTO expenses(userID, name, amount, category, created_at)
-VALUES(1, 'bubbletea', 6.7, 'drink', '2021-10-17');
+INSERT INTO expenses(expensesID, userID, name, amount, category, recurring_date, recurring)
+VALUES(7, 2, 'bubbletea', 6.7, 'drink', '2021-10-17', 1);
 
-INSERT INTO expenses(userID, name, amount, category, created_at)
-VALUES(1, 'bubbletea', 6.7, 'drink', '2021-10-16');
+INSERT INTO expenses(expensesID, userID, name, amount, category, recurring_date, recurring)
+VALUES(8, 2, 'bubbletea', 6.7, 'drink', '2021-10-18', 1);
 
 SELECT * FROM expenses;
 
 SELECT * FROM expenses WHERE recurring = 1;
+
