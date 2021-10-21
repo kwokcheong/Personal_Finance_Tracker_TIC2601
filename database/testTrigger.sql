816163fe3e75856b5e6fc8cf676ce8e29d0fb5b1
Anyer <90461487+Anyerxkp@users.noreply.github.com>
2021-10-21 10:12:06 +0800

    Update testTrigger.sql

--- database/testTrigger.sql
+++ database/testTrigger.sql
@@ -64,7 +64,8 @@ CREATE TABLE budgethead (
 );
 
 INSERT INTO users VALUES (1, 'admin', 'admin@hotmail.com', 'admin123');
-
+INSERT INTO ledger VALUES (1, 500, 500,);
+INSERT INTO incomes VALUES (1, 1, 'a',500, 'Salary','2021-03-03',1,);
 
 delimiter |
 
