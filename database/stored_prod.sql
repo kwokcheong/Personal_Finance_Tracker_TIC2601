9b3fe4e39deceb9be565b285e3660f0e81189614
Ameline <amelinechia@amelines-mbp.lan>
2021-10-25 16:43:29 +0800

    Updated sp for random bulk insertion of 300 records

--- database/stored_prod.sql
+++ database/stored_prod.sql
@@ -4,26 +4,48 @@ DROP PROCEDURE IF EXISTS `sp_insertBulkData` $$
 CREATE PROCEDURE `sp_insertBulkData`()
 BEGIN
 
+DECLARE COUNTER INT DEFAULT 1;
+
 INSERT INTO users (`name`, `email`, `password`) VALUES ('admin', 'admin@hotmail.com', 'admin');
 INSERT INTO ledger VALUES (1, 0, 0);
-
 INSERT INTO `crud_express`.`budgets` (`userID`, `category`, `budget_amount_per_month`) VALUES ('1', 'Food', RAND()*(1000-0)+10), ('1', 'Luxury', RAND()*(1000-0)+10),('1', 'Transport', RAND()*(1000-0)+10), ('1', 'Bills', RAND()*(1000-0)+10),('1', 'Others', RAND()*(1000-0)+10);
 
-INSERT INTO `crud_express`.`incomes` (`incomeID`,`userID`, `name`, `amount`, `category`, `recurring_start_date`, `recurring_end_date`, `recurring`) VALUES ('1','1', 'income1', RAND()*(1000-0)+10, 'Salary', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '1'), ('2','1', 'income2', RAND()*(1000-0)+10, 'Salary', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '0'),('3','1', 'income3', RAND()*(1000-0)+10, 'Salary', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '1'),('4','1', 'income4', RAND()*(1000-0)+10, 'Salary', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '0'),
-																																				('5','1', 'income5', RAND()*(1000-0)+10, 'Salary', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '0'),('6','1', 'income6', RAND()*(1000-0)+10, 'Salary', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '0'),('7','1', 'income7', RAND()*(1000-0)+10, 'Salary', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '0'),('8','1', 'income1', RAND()*(1000-0)+10, 'Salary', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY),  '1'),
-																																				('9','1', 'income2', RAND()*(1000-0)+10, 'Salary', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY),  '0'),('10','1', 'income3', RAND()*(1000-0)+10, 'Salary', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY),  '1'),('11','1', 'income4', RAND()*(1000-0)+10, 'Salary', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY),'0'),('12','1', 'income5', RAND()*(1000-0)+10, 'Salary', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY),  '0'),
-																																				('13','1', 'income6', RAND()*(1000-0)+10, 'Salary', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY),  '0'),('14','1', 'income7', RAND()*(1000-0)+10, 'Salary', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY),  '0');
+DROP TABLE IF EXISTS TempIncomeTableCategory; 
+CREATE TEMPORARY TABLE TempIncomeTableCategory
+(	
+	`id` INT, 
+    `category` VARCHAR(10)
+); 
 
-INSERT INTO `crud_express`.`expenses` (`expensesID`, `userID`, `name`, `amount`, `category`,`recurring_start_date`, `recurring_end_date`, `recurring`) VALUES ('1','1', 'Expense1', RAND()*(1000-0)+10, 'Food', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '0'),
-																		('2','1', 'Expense2', RAND()*(1000-0)+10, 'Utility', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '0'),('3','1', 'Expense3', RAND()*(1000-0)+10, 'Bills', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '0'),
-																		('4','1', 'Expense4', RAND()*(1000-0)+10, 'Others', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '0'),('5','1', 'Expense5', RAND()*(1000-0)+10, 'Travel', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '1'),
-																		('6','1', 'Expense6', RAND()*(1000-0)+10, 'Food', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '1'),('7','1', 'Expense7', RAND()*(1000-0)+10, 'Luxury', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '0'),
-																		('8','1', 'Expense1', RAND()*(1000-0)+10, 'Food', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '0'),('9','1', 'Expense2', RAND()*(1000-0)+10, 'Utility', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '0'),
-																		('10','1', 'Expense3', RAND()*(1000-0)+10, 'Bills', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '0'),('11','1', 'Expense4', RAND()*(1000-0)+10, 'Others', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '0'),
-																		('12','1', 'Expense5', RAND()*(1000-0)+10, 'Travel', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '1'),('13','1', 'Expense6', RAND()*(1000-0)+10, 'Food', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '1'),
-																		('14','1', 'Expense7', RAND()*(1000-0)+10, 'Luxury', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '0'),('15','1', 'Expense1', RAND()*(1000-0)+10, 'Food', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '0'),
-																		('16','1', 'Expense2', RAND()*(1000-0)+10, 'Utility', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '0'),('17','1', 'Expense3', RAND()*(1000-0)+10, 'Bills', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '0'),
-																		('18','1', 'Expense4', RAND()*(1000-0)+10, 'Others', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '0'),('19','1', 'Expense5', RAND()*(1000-0)+10, 'Travel', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '1'),
-																		('20','1', 'Expense6', RAND()*(1000-0)+10, 'Food', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '1'),('21','1', 'Expense7', RAND()*(1000-0)+10, 'Luxury', CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 500 DAY), '0');
-                                                                        
-END $$
+INSERT INTO TempIncomeTableCategory VALUES ('1','Salary'),('2','Allowance'),('3','Freelance'),('4','Others');
+
+DROP TABLE IF EXISTS TempExpensesTableCategory; 
+CREATE TEMPORARY TABLE TempExpensesTableCategory
+(	
+	`id` INT, 
+    `category` VARCHAR(10)
+); 
+
+INSERT INTO TempExpensesTableCategory VALUES ('1','Food'),('2','Luxury'),('3','Travel'),('4','Bills'),('5','Others'),('6','Utility');
+
+WHILE COUNTER <= 300 DO
+	CALL sp_insertData(COUNTER,(SELECT category FROM TempIncomeTableCategory ORDER BY RAND() LIMIT 1),(SELECT category FROM TempExpensesTableCategory ORDER BY RAND() LIMIT 1));
+    SET COUNTER = COUNTER + 1;
+END WHILE;
+
+END$$
+DELIMETER;
+
+DELIMETER $$
+DROP PROCEDURE IF EXISTS `sp_insertData` $$
+CREATE PROCEDURE `sp_insertData`(COUNTER INT, CATEGORY_INCOME TEXT, CATEGORY_EXPENSE TEXT)
+BEGIN
+
+	INSERT INTO `crud_express`.`incomes` (`incomeID`,`userID`, `name`, `amount`, `category`, `recurring_start_date`, `recurring_end_date`, `recurring`) VALUES 
+	(COUNTER,'1', CONCAT('Income',COUNTER), RAND()*(1000-0)+10, CATEGORY_INCOME, CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 365 DAY), FLOOR(RAND()*(1-0+1))+0);
+
+	INSERT INTO `crud_express`.`expenses` (`expensesID`, `userID`, `name`, `amount`, `category`,`recurring_start_date`, `recurring_end_date`, `recurring`) VALUES 
+	(COUNTER,'1', CONCAT('Expense',COUNTER), RAND()*(1000-0)+10, CATEGORY_EXPENSE, CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, DATE_ADD(CURRENT_DATE - INTERVAL FLOOR(RAND() * 100) DAY, INTERVAL 365 DAY), FLOOR(RAND()*(1-0+1))+0);
+
+END$$
+DELIMETER;
